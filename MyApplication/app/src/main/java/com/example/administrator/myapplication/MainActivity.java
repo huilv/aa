2c53e33d5d9304ddba1b83b482ccb4df4a29f8d4
huilv <1204483693@qq.com>
2016-05-14 16:09:15 +0800

    'aaaaaaaaaa'

--- MyApplication/app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -8,12 +8,12 @@ public class MainActivity extends AppCompatActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		setContentView(R.layout.activity_main);
 		setContentView(R.layout.activity_main);
 		setContentView(R.layout.activity_main);
 		setContentView(R.layout.activity_main);
 		setContentView(R.layout.activity_main);
-		
+		//zfsadfasdfsadfasddsfsdsfsds
 	}
 }
