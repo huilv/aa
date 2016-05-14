b41b933c4b95cfa7c54534e430203e1bba88d059
huilv <1204483693@qq.com>
2016-05-14 20:13:46 +0800

    2222222222222222

--- MyApplication/app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		//1111111111111111111
+		//2222222222222222
+
 	}
 }
