eff7eccc705ce9c5768ee0fc82135607caa3abfa
huilv <1204483693@qq.com>
2016-05-14 16:14:01 +0800

    'fffffffffffffffffffffff'

--- MyApplication/app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+		//sdfsadfsdfsdf
 		setContentView(R.layout.activity_main);
 		setContentView(R.layout.activity_main);
 		setContentView(R.layout.activity_main);
