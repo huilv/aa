202d8c797e5d1c4a74d6d49422ccdfe654c3d296
huilv <1204483693@qq.com>
2016-05-14 22:37:04 +0800

    'ddddddddddddddddddddddddddddd'

--- MyApplication/app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -9,11 +9,5 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		//1111111111111111111
-		//2222222222222222
-//33333333333333333
-		//4444444444444
-		//5555
-
 	}
 }
