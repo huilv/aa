cca2808ad2d45e796e760ead3a713849f3ec0b39
huilv <1204483693@qq.com>
2016-05-14 16:47:32 +0800

    'sadfsd'

--- MyApplication/app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
+		//zfsadfasdfsadfasddsfsdsfsds
 		setContentView(R.layout.activity_main);
 		setContentView(R.layout.activity_main);
 		setContentView(R.layout.activity_main);
